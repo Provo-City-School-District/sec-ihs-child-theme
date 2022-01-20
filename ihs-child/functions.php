264819610a4f5d46948bde5def3a0c42d2a0831c
Espi <joshespi@gmail.com>
2022-01-20 14:12:36 -0700

    added favicon

--- ihs-child/functions.php
+++ ihs-child/functions.php
@@ -9,6 +9,18 @@ function my_theme_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 /*==========================================================================================
+// Favicon
+============================================================================================*/
+function pcsd_add_favicon(){ ?>
+	<!-- Custom Favicons -->
+	<link rel="shortcut icon" href="<?php echo get_theme_file_uri(); ?>/assets/images/favicon.png"/>
+	<link rel="apple-touch-icon" href="<?php echo get_theme_file_uri(); ?>/assets/images/favicon.png">
+	<?php }
+//add the favicon link to the live site head
+add_action('wp_head','pcsd_add_favicon');
+//add the favicon to the login page
+add_action('login_head','pcsd_add_favicon');
+/*==========================================================================================
 Custom Excerpt
 ============================================================================================*/
 function get_excerpt(){
