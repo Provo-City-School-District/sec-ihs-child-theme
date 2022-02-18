e106a790594ab7d0786bb07a2dcdd14913d87daa
Espi <joshespi@gmail.com>
2022-02-18 12:39:24 -0700

    updated "How to Schedule a Substitute" link reference

--- ihs-child/footer.php
+++ ihs-child/footer.php
@@ -7,7 +7,7 @@
 				 	
 				 	<li><a href="https://helpdesk.provo.edu:8443/helpdesk/WebObjects/Helpdesk.woa"><img src="https://timpview.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/work-order.svg" alt="" />Open a work order</a></li>
 				 	<li><a href="http://mail.google.com"><img src="https://timpview.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/email.svg" alt="" />Email Access</a></li>
-				 	<li><a href="https://independence.provo.edu/how-to-schedule-a-substitute/"><img src="https://timpview.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/find-a-substitute.svg" alt="" />How to Schedule a Substitute</a></li>
+				 	<li><a href="https://login.frontlineeducation.com/login?signin=c4f172530351d39768e7d4ddf134db58&productId=ABSMGMT&clientId=ABSMGMT#/login"><img src="https://timpview.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/find-a-substitute.svg" alt="" />How to Schedule a Substitute</a></li>
 				 	<li><a href="https://grades.provo.edu/teachers/pw.html"><img src="https://timpview.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/power-school.png" alt="" />PowerTeacher Login</a></li>
 				 	<li><a href="https://provo.instructure.com/login/saml/13"><img src="https://timpview.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/file-white.svg" alt="" />Canvas Login</a></li>
 				 	<li class="employeelink"><a href="https://employee.provo.edu/"><img src="https://timpview.provo.edu/wp-content/uploads/2017/12/pcsd-logo-website-header-branding.png" alt="" />Employee Support Website</a></li>
