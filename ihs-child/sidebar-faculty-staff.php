c4752989b7ff11ddeb760cf8c2d744f77357108e
Espi <joshespi@gmail.com>
2022-04-19 13:49:28 -0600

    white space rollup

--- ihs-child/sidebar-faculty-staff.php
+++ ihs-child/sidebar-faculty-staff.php
@@ -1,5 +1,5 @@
 <aside id="mainSidebar">
-	
+
 	<?php
 		//logic to remove the form on the ESP listing because Rob requested "-- Remove the search field because there are only 2 staff listed on that page" 09-11-2020
 		if(!is_page(184)){
@@ -8,9 +8,9 @@
 		<label for="dsearch" class="hidden" id="directorySearch">Directory Search: </label>
 		<input type="text" name="dsearch" class="text-input" aria-labelledby="directorySearch" id="sidebar-filter" value="" placeholder="<?php if(is_page($post = '40')){echo 'Search Administration & Main Office';}elseif(is_page($post = '42744')){ echo 'Search Counseling Staff'; }elseif(is_page($post = '72')){ echo 'Search Teachers'; }else{ echo 'Search our Staff...';} ?>" />
 		<img class="directorySearchIcon" src="//globalassets.provo.edu/image/icons/search-lt.svg" alt="" />
-	</section>		
+	</section>
 			<?php
-		}	
+		}
 	?>
 	<section>
 		<h1>Teachers &amp; Staff</h1>
@@ -32,8 +32,8 @@
 				<li class="int"><a href="<?php echo get_home_url(); ?>/our-teachers-staff/teacher-course-directory/social-studies/">Social Studies</a></li>
 				<li class="int"><a href="<?php echo get_home_url(); ?>/our-teachers-staff/teacher-course-directory/special-education/">Special Education</a></li>
 			</ul>
-	</section>		
+	</section>
 			<?php
-		}	
+		}
 	?>
 </aside>
